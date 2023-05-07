4bb2ca158c53ec0e1353b4038bac7adad42c3403
aestas <hawhawba@gmail.com>
2023-05-07 21:40:29 +0900

    init: tailwind css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,21 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="flex flex-col items-center">
+    <img src="/tailwindcss.svg" class="logo" alt="Tailwind CSS logo" />
+    <p class="text-3xl text-cyan-200">{{ msg }}</p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+</script>
+
 <style scoped>
-.read-the-docs {
-  color: #888;
+.logo {
+  @apply w-24;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 1em #78cbffaa);
 }
 </style>
